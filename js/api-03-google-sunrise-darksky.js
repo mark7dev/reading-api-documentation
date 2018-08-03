8163dc61ff87f05b961df5abb0ab532d9f78b3b9
mark7dev <mcamacho_ce7@live.com.mx>
2018-08-03 00:53:04 -0500

    version02

--- js/api-03-google-sunrise-darksky.js
+++ js/api-03-google-sunrise-darksky.js
@@ -22,15 +22,51 @@
 
 const answerElement_apimashup_1 = document.getElementById('apimashup-1')
 
+var API_URL_G1 = 'https://maps.googleapis.com/maps/api/geocode/json?address=Montreal'
+
+request
+  .get(API_URL_G1)
+  .then(function(response) {
+    var information = response.body.results;
+    information.forEach(function(date) {
+      var lat = date.geometry.location.lat;
+      var lng = date.geometry.location.lng;
+
+      answerElement_apimashup_1.innerHTML += '<p>Lat: ' + lat + '</p>' + '<p>Lng: ' + lng + '</p>';
+    })
+  })
 
 
 
 //========================================================================
 //  (2) What time does the sun set in Montreal (based on the Google geocode coordinates)?
 
-const answerElement_apimashup_2 = document.getElementById('apimashup-2')
+function getSunset(latitude, longitude) {
+
+	const answerElement_apimashup_2 = document.getElementById('apimashup-2')
+
+    var API_URL_G2 = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=today`;
+
+    request
+    .get(API_URL_G2)
+	  .then(function(response){
+		  var information = response.body.results;
+		  var sunsetTime = information.sunset;
+		  answerElement_apimashup_2.innerHTML += '<p>Sunset: ' + sunsetTime + '</p>';	
+		})
+}
 
 
+request
+  .get('https://maps.googleapis.com/maps/api/geocode/json?address=Montreal')
+  .then(function(response){
+    var information = response.body.results
+    information.forEach(function(date){
+    	var lat = date.geometry.location.lat;
+    	var lng = date.geometry.location.lng;
+    	getSunset(lat, lng);						   
+    });
+  })
 
 
 
@@ -38,9 +74,32 @@ const answerElement_apimashup_2 = document.getElementById('apimashup-2')
 //========================================================================
 //  (3) What is the weekly weather forecast in Montreal? (look for summary property in 'daily')
 
-const answerElement_apimashup_3 = document.getElementById('apimashup-3')
+function getWeather(latitude, longitude) {
 
+	const answerElement_apimashup_3 = document.getElementById('apimashup-3')
 
+	var API_KEY = '012be0be9ed18413001495614a5fac78';
+    var API_URL_G3 = `https://api.darksky.net/forecast/${API_KEY}/${latitude},${longitude}`;
+
+    request
+    .get(API_URL_G3)
+	  .then(function(response){
+		  var information = response.body;
+		  var weather = information.daily.summary;
+		  answerElement_apimashup_3.innerHTML = '<p>' + weather + '</p>';
+		})
+}
+
+request
+  .get('https://maps.googleapis.com/maps/api/geocode/json?address=Montreal')
+  .then(function(response){
+    var information = response.body.results
+    information.forEach(function(date){
+    	var lat = date.geometry.location.lat;
+    	var lng = date.geometry.location.lng;
+    	getWeather(lat, lng);						   
+    });
+  })
 
 
 //
--- js/api-04-tv-maze.js
+++ js/api-04-tv-maze.js
@@ -1,11 +1,41 @@
 /*
 ##### TV Maze
 https://www.tvmaze.com/ap
-
-1. What is the average rating for the show Better Call Saul?
-
-2. When was the premiere date for the 9th season of Friends?
-
-3. How many shows has actor Andrew Grimes (of the Walking Dead) appeared in?
 */
-*/
+
+// 1. What is the average rating for the show Better Call Saul?
+
+const answerElement_tvmaze_1 = document.querySelector('#tvmaze-1')
+
+request
+.get('http://api.tvmaze.com/shows/618')
+.then(function(response){
+    var information = response.body;
+    var rating = information.rating.average;
+    answerElement_tvmaze_1.innerHTML += '<p>' + rating + '</p>';
+})
+
+// 2. When was the premiere date for the 9th season of Friends?
+
+const answerElement_tvmaze_2 = document.querySelector('#tvmaze-2')
+
+request
+.get('http://api.tvmaze.com/shows/431/episodebynumber?season=9&number=1')
+.then(function(response){
+    var information = response.body;
+    answerElement_tvmaze_2.innerHTML += '<p>' + information.airdate + '</p>';
+})
+
+
+// 3. How many shows has actor Andrew Grimes (of the Walking Dead) appeared in?
+
+const answerElement_tvmaze_3 = document.querySelector('#tvmaze-3')
+
+request
+.get('http://api.tvmaze.com/people/10257/castcredits')
+.then(function(response){
+    var information = response.body;
+    var shows = information.length;
+    answerElement_tvmaze_3.innerHTML += '<p>' + shows + '</p>';
+})
+
